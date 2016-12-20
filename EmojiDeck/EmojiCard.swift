a5a599b5d9f570a8ddbc6baaca3ffe152ebb455c
Marty Hernandez Avedon <martyavedon@gmail.com>
2016-12-20 18:01:53 -0500

    working on buttons in view controller, put emojicard model on ice

--- EmojiDeck/EmojiCard.swift
+++ EmojiDeck/EmojiCard.swift
@@ -8,37 +8,37 @@
 
 import UIKit
 
-protocol PlayingCard {
-    var suit: Suit { get }
-    var num: Number { get }
-}
+//protocol PlayingCard {
+  //  var suit: Suit { get }
+  //  var num: Number { get }
+//}
 
-class EmojiCard: UIView, PlayingCard {
-    var topNumberLabel: UILabel!
-    var topSuitLabel: UILabel!
-    var bottomNumberLabel: UILabel!
-    var bottomSuitLabel: UILabel!
-    var middleImage: UIImage!
+//class EmojiCard: UIView, PlayingCard {
+//    var topNumberLabel: UILabel!
+//    var topSuitLabel: UILabel!
+//    var bottomNumberLabel: UILabel!
+//    var bottomSuitLabel: UILabel!
+//    var middleImage: UIImage!
     
-    let suit: Suit
-    let num: Number
+ //   let suit: Suit
+ //   let num: Number
     
-    init(frame: CGRect, suit:Suit, num: Number) {
-        self.suit = suit
-        self.num = num
-        
-        super.init(frame: frame)
-    }
+ //   init(frame: CGRect, suit:Suit, num: Number) {
+ //       self.suit = suit
+ //       self.num = num
+ //
+ //       super.init(frame: frame)
+ //   }
     
-    required init?(coder aDecoder: NSCoder) {
+ //   required init?(coder aDecoder: NSCoder) {
         
-        super.init(coder: aDecoder)
+ //       super.init(coder: aDecoder)
         
-        layer.borderWidth = 2
-        layer.borderColor = UIColor.black.cgColor
+ //       layer.borderWidth = 2
+ //       layer.borderColor = UIColor.black.cgColor
         
         // this bit turns the suit & num upside down -- the units are in radians
-        bottomNumberLabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi)
-        bottomSuitLabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi)
-    }
-}
+ //       bottomNumberLabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi)
+ //       bottomSuitLabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi)
+ //   }
+// }
--- EmojiDeck/EmojiCardViewController.swift
+++ EmojiDeck/EmojiCardViewController.swift
@@ -10,26 +10,99 @@ import UIKit
 
 class EmojiCardViewController: UIViewController {
 
+    var drawOneButton = UIButton(type: UIButtonType.system)
+    var removeOneButton = UIButton(type: UIButtonType.system)
+    var showStackButton = UIButton(type: UIButtonType.system)
+    var removeAllButton = UIButton(type: UIButtonType.system)
+    //var card = EmojiCard?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        self.view.backgroundColor = .blue
+        
+        let _ = [
+            self.removeAllButton,
+            self.showStackButton,
+            self.removeOneButton,
+            self.drawOneButton
+        ].map { $0.translatesAutoresizingMaskIntoConstraints = false }
+        
+        self.drawOneButton.setTitle(" Draw Card ", for: .normal)
+        self.drawOneButton.setTitleColor(.white, for: .normal)
+        self.drawOneButton.layer.borderColor = UIColor.white.cgColor
+        self.drawOneButton.layer.borderWidth = 1
+        
+        self.removeOneButton.setTitle(" Remove Card ", for: .normal)
+        self.removeOneButton.setTitleColor(.white, for: .normal)
+        self.removeOneButton.layer.borderColor = UIColor.white.cgColor
+        self.removeOneButton.layer.borderWidth = 1
+        
+        self.removeAllButton.setTitle(" Remove All ", for: .normal)
+        self.removeAllButton.setTitleColor(.white, for: .normal)
+        self.removeAllButton.layer.borderColor = UIColor.white.cgColor
+        self.removeAllButton.layer.borderWidth = 1
+        
+        self.showStackButton.setTitle(" Show Stack ", for: .normal)
+        self.showStackButton.setTitleColor(.white, for: .normal)
+        self.showStackButton.layer.borderColor = UIColor.white.cgColor
+        self.showStackButton.layer.borderWidth = 1
+        
+        self.view.addSubview(drawOneButton)
+        self.view.addSubview(removeOneButton)
+        self.view.addSubview(removeAllButton)
+        self.view.addSubview(showStackButton)
+        
+        let _ = [
+            removeOneButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            removeOneButton.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            drawOneButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            drawOneButton.bottomAnchor.constraint(equalTo: removeOneButton.bottomAnchor, constant: -38),
+            removeAllButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            removeAllButton.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            showStackButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            showStackButton.bottomAnchor.constraint(equalTo: removeAllButton.bottomAnchor, constant: -38)
+            ].map{ $0.isActive = true }
+        
+        self.drawOneButton.addTarget(self, action: #selector(didPressDrawOneButton(sender:)), for: .touchUpInside)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        print("layout subviews!!!")
     }
-    */
-
+    
+    func didPressDrawOneButton(sender: UIButton) {
+        print("Did press button.")
+        
+        // create a destination vc
+        
+        let newVC = EmojiCardViewController()
+        newVC.view.backgroundColor = .green
+        
+        // move to our destination vc
+        
+        // present it modally
+        //self.present(greenVC, animated: true, completion: nil)
+        
+        // have the nav manage it
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("view will appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("view did disappear")
+    }
+    
 }
