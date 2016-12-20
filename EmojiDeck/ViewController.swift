8b7a4fcb62e5be4d594cc7681ada81b07f20d9f2
Marty Hernandez Avedon <martyavedon@gmail.com>
2016-12-20 15:50:53 -0500

    Initial commit

---
+++ EmojiDeck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojiDeck
+//
+//  Created by Marty Avedon on 12/20/16.
+//  Copyright © 2016 Marty Hernandez Avedon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
