a348006a5741f78776a81d6bb8ad785ec510fd16
Marty Hernandez Avedon <martyavedon@gmail.com>
2016-12-20 17:20:54 -0500

    adding properties and trying to set initializers for emojicard

--- EmojiDeck/EmojiCard.swift
+++ EmojiDeck/EmojiCard.swift
@@ -6,20 +6,39 @@
 //  Copyright © 2016 Marty Hernandez Avedon. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 protocol PlayingCard {
     var suit: Suit { get }
     var num: Number { get }
 }
 
-class EmojiCard: PlayingCard {
+class EmojiCard: UIView, PlayingCard {
+    var topNumberLabel: UILabel!
+    var topSuitLabel: UILabel!
+    var bottomNumberLabel: UILabel!
+    var bottomSuitLabel: UILabel!
+    var middleImage: UIImage!
+    
     let suit: Suit
     let num: Number
     
-    init(suit:Suit, num: Number) {
+    init(frame: CGRect, suit:Suit, num: Number) {
         self.suit = suit
         self.num = num
+        
+        super.init(frame: frame)
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+        
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.black.cgColor
+        
+        // this bit turns the suit & num upside down -- the units are in radians
+        bottomNumberLabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi)
+        bottomSuitLabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi)
+    }
 }
