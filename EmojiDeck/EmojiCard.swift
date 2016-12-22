d07f1632ca81d228ff888d599eab3b8ee461b21c
Marty Hernandez Avedon <martyavedon@gmail.com>
2016-12-22 11:56:11 -0500

    got card to appear, but no labels yet

--- EmojiDeck/EmojiCard.swift
+++ EmojiDeck/EmojiCard.swift
@@ -25,23 +25,49 @@ class EmojiCard: UIView, PlayingCard {
     override init(frame: CGRect) {
  
         super.init(frame: frame)
-        
-        //self.widthAnchor.constraint(equalToConstant: 250).isActive = true
-        //self.heightAnchor.constraint(equalToConstant: 350).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
         
         super.init(coder: aDecoder)
-        
-        self.layer.borderWidth = 2
-        self.layer.borderColor = self.suit.color().cgColor
-        
-        // this bit turns the suit & num upside down -- the units are in radians
-        bottomNumberLabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi)
-        bottomSuitLabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi)
     }
     
-    
+    func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.borderWidth = 2
+        layer.borderColor = suit.color().cgColor
+        
+        backgroundColor = .white
+        
+        widthAnchor.constraint(equalToConstant: 250).isActive = true
+        heightAnchor.constraint(equalToConstant: 350).isActive = true
+        
+//        addSubview(topNumberLabel)
+//        addSubview(bottomNumberLabel)
+//        addSubview(topSuitLabel)
+//        addSubview(bottomSuitLabel)
+//        
+//        let _ = [
+//            topNumberLabel,
+//            topSuitLabel,
+//            bottomNumberLabel,
+//            bottomSuitLabel
+//            ].map { $0.translatesAutoresizingMaskIntoConstraints = false }
+        
+        topNumberLabel?.text = num.cornerLabel()
+        bottomNumberLabel?.text = num.cornerLabel()
+        topSuitLabel?.text = suit.symbol()
+        bottomNumberLabel?.text = suit.symbol()
+        
+        topSuitLabel?.textColor = suit.color()
+        topNumberLabel?.textColor = suit.color()
+        bottomSuitLabel?.textColor = suit.color()
+        bottomNumberLabel?.textColor = suit.color()
+        
+        // this bit turns the suit & num upside down -- the units are in radians
+        bottomNumberLabel?.transform = CGAffineTransform(rotationAngle: -CGFloat.pi)
+        bottomSuitLabel?.transform = CGAffineTransform(rotationAngle: -CGFloat.pi)
+    }
     
  }
